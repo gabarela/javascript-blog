675937a9a7de65c9ae579f0081ae686b8568a817
Gabriela Cichowicz <gabriela.cichowicz@gmail.com>
2020-03-05 22:24:12 +0100

    Tags / upadates in html & js - not finished

--- js/script.js
+++ js/script.js
@@ -47,7 +47,8 @@ function titleClickHandler(event) {
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks() {
 
@@ -85,7 +86,7 @@ function generateTitleLinks() {
     /* insert link into titleList */
 
     html = html + linkHTML;
-    
+
   }
 
   titleList.innerHTML = html;
@@ -100,3 +101,51 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
+function generateTags(){
+
+  /* find all articles */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+
+for (let article of articles) {
+
+    /* find tags wrapper */
+
+const tagWrapper = article.querySelector(optArticleTagsSelector);
+
+    /* make html variable with empty string */
+
+    let html = '';
+
+    /* get tags from data-tags attribute */
+
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
+
+    /* split tags into array */
+
+    const articleTagsArray = articleTags.split(' ');
+
+    /* START LOOP: for each tag */
+for(let tag of articleTagsArray) {
+      /* generate HTML of the link */
+
+
+      const linkHTML = '<li><a href="#'+ articleTags + ' ">' + articleTagsArray + '</a></li>';
+      console.log(linkHTML);
+      /* add generated code to html variable */
+      let html = linkHTML;
+      html = html + linkHTML;
+}
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
+}
