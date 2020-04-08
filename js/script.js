5922287f5a8c7c6ca458d42f9a3363f3d74b9474
Gabriela Cichowicz <gabriela.cichowicz@gmail.com>
2020-04-08 13:41:43 +0200

    generateTags cloud / not  finished

--- js/script.js
+++ js/script.js
@@ -44,7 +44,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';
 
 function generateTitleLinks(customSelector = ''){
 
@@ -90,6 +91,8 @@ function generateTitleLinks(customSelector = ''){
 }
 
 generateTitleLinks();
+
+
 
 function generateTags() {
 
@@ -273,3 +276,54 @@ function addClickListenersToAuthors() {
 }
 
 addClickListenersToAuthors();
+
+function generateTags(){
+ /* [NEW] create a new variable allTags with an empty object */
+ let allTags = {};
+
+ /* find all articles */
+ const articles = document.querySelectorAll(optArticleSelector);
+
+ /* START LOOP: for every article: */
+ for (let article of articles) {
+
+ /* find tags wrapper */
+ const tagsWrapper = article.querySelector(optArticleTagsSelector);
+
+ /* make html variable with empty string */
+ let html = '';
+
+ /* get tags from data-tags attribute */
+ const articleTags = article.getAttribute('data-tags');
+
+ /* split tags into array */
+ const articleTagsArray = articleTags.split(' ');
+
+ /* START LOOP: for each tag */
+ for (let tag of articleTagsArray) {
+
+ /* generate HTML of the link */
+ const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span> </a></li>'+ ' ';
+
+ /* add generated code to html variable */
+   html = html + linkHTML;
+
+ /* [NEW] check if this link is NOT already in allTags */
+ if(allTags.indexOf(linkHTML) == -1){
+ /* [NEW] add generated code to allTags array */
+ allTags.push(linkHTML);
+ }
+ /* END LOOP: for each tag */
+}
+ /* insert HTML of all the links into the tags wrapper */
+ tagsWrapper.innerHTML = html;
+
+ /* END LOOP: for every article: */
+}
+
+ /* [NEW] find list of tags in right column */
+ const tagList = document.querySelector('.tags');
+
+ /* [NEW] add html from allTags to tagList */
+ tagList.innerHTML = allTags.join(' ');
+}
